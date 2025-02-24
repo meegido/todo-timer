faa503a7bee2be72d7134e643985e469c05c71cd
Mercedes Egido Vallejo <mercedes.egido@hey.com>
2025-02-24 15:47:49 +0100

    adaptataive cards

--- src/todo-timer.tsx
+++ src/todo-timer.tsx
@@ -12,6 +12,9 @@ const data = [
   { title: 'Read the article about Testing Library', id: 'i234234' },
   { title: 'UI Benchmark', id: '3w4hkljsd' },
   { title: 'Split the tasks into small slices', id: '3549349348' },
+  { title: 'Understand container queries', id: 'i2¡3453244234' },
+  { title: 'Understand mix-max widht', id: '30909w4hkljsd' },
+  { title: `Don't forget to do a proper slicing`, id: '35493493432238' },
 ];
 
 function TodoTimer() {
