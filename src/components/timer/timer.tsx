a9d21d37d8f5ee3d1faea57caae67adf3122d7e4
Mercedes Egido Vallejo <mercedes.egido@hey.com>
2025-02-25 17:01:57 +0100

    minutes and seconds formatting when the countdown is running

--- src/components/timer/timer.tsx
+++ src/components/timer/timer.tsx
@@ -8,13 +8,13 @@ interface TimeLeft {
 
 const Timer = () => {
   const [timeLeft, setTimeLeft] = React.useState<TimeLeft>({
-    minutes: 25,
+    minutes: 1,
     seconds: 0,
   });
   const intervalRef = React.useRef<number | null>(0);
 
   const duration = timeLeft.minutes * 60 * 1000 + timeLeft.seconds * 1000; // Convert to milliseconds
-  const durationTimestamp = new Date().getTime() + duration; // Calculate the target timestamp
+  const durationTimestamp = new Date().getTime() + duration; // Calculate the target timestamp - future
 
   React.useEffect(() => {
     const currentTimeStamp = new Date().getTime();
@@ -30,9 +30,17 @@ const Timer = () => {
       });
     };
 
-    if (remaining > 0) {
-      intervalRef.current = window.setInterval(updateCountdown, 1000);
+    if (remaining <= 0 && intervalRef.current) {
+      window.clearInterval(intervalRef.current);
+      setTimeLeft(() => {
+        return {
+          minutes: 0,
+          seconds: 0,
+        };
+      });
     }
+
+    intervalRef.current = window.setInterval(updateCountdown, 1000);
 
     return () => {
       if (intervalRef.current) {
@@ -46,14 +54,18 @@ const Timer = () => {
       <div className={styles.timer__wrapper}>
         <div className={styles.time__wrapper}>
           <span aria-label={`${timeLeft.minutes} minutes left`}>
-            {timeLeft.minutes}
+            {timeLeft.minutes.toString().length <= 1
+              ? `${'0' + timeLeft.minutes}`
+              : timeLeft.minutes}
           </span>
           <span>Minutes</span>
         </div>
         <span className={styles.divider}>:</span>
         <div className={styles.time__wrapper}>
           <span aria-label="00 seconds left">
-            {timeLeft.seconds === 0 ? '00' : timeLeft.seconds}
+            {timeLeft.seconds.toString().length <= 1
+              ? `${'0' + timeLeft.seconds}`
+              : timeLeft.seconds}
           </span>
           <span>Seconds</span>
         </div>
