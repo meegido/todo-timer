a834819df860210a273074d8be54810bac989445
Mercedes Egido Vallejo <mercedes.egido@hey.com>
2025-02-26 16:59:04 +0100

    styled timer controls in todo

--- src/components/todo-item/todo-item.tsx
+++ src/components/todo-item/todo-item.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Todo } from '../../todo-timer';
 import styles from './todo-item.module.css';
+import TimerControls from '../../shared/timer-controls/timer-controls';
 
 interface TodoItemProps {
   todo: Todo;
@@ -26,68 +27,75 @@ const TodoItem = ({ todo, onUpdateTodo }: TodoItemProps) => {
 
   return (
     <div className={styles.card}>
-      {
+      <section className={styles.content__wrapper}>
         <div className={styles.check__wrapper}>
-          <label htmlFor="done" className={styles.visually__hidden}>
+          <label
+            htmlFor={`done-${todo.id}`}
+            className={styles.visually__hidden}
+          >
             Check or uncheck the todo as done
           </label>
           <input
             type="checkbox"
             name="done"
-            id="done"
+            id={`done-${todo.id}`}
             value="done"
             aria-label="Check or uncheck the todo as done"
             checked={isTodoDone}
             onChange={() => setIsTodoDone(!isTodoDone)}
           />
         </div>
-      }
-      {isEditMode ? (
-        <div className={styles.input__wrapper}>
-          <label className={styles.visually__hidden} htmlFor="todo">
-            Edit your todo title
-          </label>
-          <textarea
-            ref={inputRef}
-            name="todo"
-            id="todo"
-            aria-label="Edit your todo title"
-            placeholder={todo.title}
-            maxLength={200}
-            rows={1}
-            value={editTodoValue || todo.title}
-            onChange={(event) => {
-              setEditTodoValue(event.target.value);
-            }}
-            onKeyDown={(event) => {
-              if (event.key === 'Enter') {
-                event.preventDefault();
-                handleSave();
-              }
 
-              if (event.key === 'Escape') {
+        {isEditMode ? (
+          <div className={styles.input__wrapper}>
+            <label className={styles.visually__hidden} htmlFor="todo">
+              Edit your todo title
+            </label>
+            <textarea
+              ref={inputRef}
+              name="todo"
+              id="todo"
+              aria-label="Edit your todo title"
+              placeholder={todo.title}
+              maxLength={200}
+              rows={1}
+              value={editTodoValue || todo.title}
+              onChange={(event) => {
+                setEditTodoValue(event.target.value);
+              }}
+              onKeyDown={(event) => {
+                if (event.key === 'Enter') {
+                  event.preventDefault();
+                  handleSave();
+                }
+
+                if (event.key === 'Escape') {
+                  event.preventDefault();
+                  setIsEditMode(false);
+                  setEditTodoValue(todo.title);
+                }
+              }}
+              onBlur={(event) => {
                 event.preventDefault();
                 setIsEditMode(false);
                 setEditTodoValue(todo.title);
-              }
-            }}
-            // onBlur={(event) => {
-            //   event.preventDefault();
-            //   setIsEditMode(false);
-            //   setEditTodoValue(todo.title);
-            // }}
-          />
-        </div>
-      ) : (
-        <p
-          className={isTodoDone ? styles.done : styles.todo__title}
-          key={todo.id}
-          aria-label="Todo title"
-          onClick={() => setIsEditMode(true)}
-        >
-          {todo.title}
-        </p>
-      )}
+              }}
+            />
+          </div>
+        ) : (
+          <p
+            className={isTodoDone ? styles.done : styles.todo__title}
+            key={todo.id}
+            aria-label="Todo title"
+            onClick={() => setIsEditMode(true)}
+          >
+            {todo.title}
+          </p>
+        )}
+      </section>
+      <section className={styles.controls__wrapper}>
+        <TimerControls />
+      </section>
     </div>
   );
 };
