0a2dbb085ec5a9036d180772cd0f2dafe061c897
Mercedes Egido Vallejo <mercedes.egido@hey.com>
2025-03-12 12:23:37 +0100

    fixing fixture

--- src/hooks/use-timer.test.tsx
+++ src/hooks/use-timer.test.tsx
@@ -2,18 +2,38 @@ import { render, screen, waitFor, within } from '@testing-library/react';
 import { beforeEach, describe, expect, it, vi } from 'vitest';
 import TimerProvider from '../providers/timer-provider';
 import TodoTimer from '../todo-timer';
-import { Todo, TodoClient } from '../todo-client';
+import { Todo, TodoClient, TodoVariant } from '../todo-client';
 import userEvent from '@testing-library/user-event';
 
 class FakeTodoClient implements TodoClient {
   retrieve = (): Todo[] => {
     return [
-      { title: 'Read the article about Testing Library', id: 'i234234' },
-      { title: 'UI Benchmark', id: '3w4hkljsd' },
-      { title: 'Split the tasks into small slices', id: '3549349348' },
-      { title: 'Understand container queries', id: 'i2¡3453244234' },
-      { title: 'Understand mix-max widht', id: '30909w4hkljsd' },
-      { title: `Don't forget to do a proper slicing`, id: '35493493432238' },
+      {
+        title: 'Read the article about Testing Library',
+        id: 'i234234',
+        variant: TodoVariant.INACTIVE,
+      },
+      { title: 'UI Benchmark', id: '3w4hkljsd', variant: TodoVariant.INACTIVE },
+      {
+        title: 'Split the tasks into small slices',
+        id: '3549349348',
+        variant: TodoVariant.INACTIVE,
+      },
+      {
+        title: 'Understand container queries',
+        id: 'i2¡3453244234',
+        variant: TodoVariant.INACTIVE,
+      },
+      {
+        title: 'Understand mix-max widht',
+        id: '30909w4hkljsd',
+        variant: TodoVariant.INACTIVE,
+      },
+      {
+        title: `Don't forget to do a proper slicing`,
+        id: '35493493432238',
+        variant: TodoVariant.INACTIVE,
+      },
     ];
   };
 }
